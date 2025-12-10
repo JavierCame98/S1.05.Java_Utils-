3b644c61dfeac575717b7c5d5cd84f89c739efa0
Javier Castillo <javiercame98@gmail.com>
2025-12-10 16:20:25 +0100

    feat: creating a method to sort alphabetically a directory

--- src/main/java/ListAlphabeticallyTheDirectory/DirectoryAlphabetically.java
+++ src/main/java/ListAlphabeticallyTheDirectory/DirectoryAlphabetically.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Comparator;
 import java.util.Objects;
+import java.util.stream.Stream;
 
 public class DirectoryAlphabetically {
 
@@ -22,6 +24,17 @@ public class DirectoryAlphabetically {
         Files.createDirectories(path);
         System.out.println("Directory created: " + path.toAbsolutePath());
         return true;
+    }
+
+    public void listAlphabeticallyTheDirectory(Path listDirectory) throws IOException{
+        if(!Files.isDirectory(listDirectory)){
+            throw new IllegalArgumentException("The File is not a directory");
+        }
+
+        try(Stream<Path> stream = Files.list(listDirectory)){
+            stream.sorted(Comparator.comparing(path -> path.getFileName().toString(), String.CASE_INSENSITIVE_ORDER))
+                    .forEach(path -> System.out.println(path.getFileName()));
+        }
     }
 
 
